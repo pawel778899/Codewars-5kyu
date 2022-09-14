ec29a7af385d33b5f466d24e918999df89c1970f
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 13:54:08 +0200

    Create IsMyFriendCheating?.java

---
+++ IsMyFriendCheating?/IsMyFriendCheating?.java
@@ -0,0 +1,59 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class RemovedNumbers {
+
+    static List<long[]> removNb(long n) {
+        long sum = (1 + n) * n / 2;
+        LinkedList<long[]> result = new LinkedList<>();
+
+        long minimum = (n * (n - 1) / 2) / (n + 1);
+        long maximum = (long) (Math.sqrt(sum + 1) - 1);
+
+        for (long i = maximum; i >= minimum; --i) {
+            long j = (sum - i) / (i + 1);
+            if (i * j == sum - i - j) {
+                result.addFirst(new long[]{i, j});
+                result.addLast(new long[]{j, i});
+            }
+        }
+
+        return result;
+    }
+  }
+
+
+
+// DESCRIPTION:
+// A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
+// Within that sequence, he chooses two numbers, a and b.
+// He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+// Given a number n, could you tell me the numbers he excluded from the sequence?
+// The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+
+// [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+// with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+
+// [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order of the "a".
+
+// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
+
+// Examples:
+// removNb(26) should return [(15, 21), (21, 15)]
+// or
+// removNb(26) should return { {15, 21}, {21, 15} }
+// or
+// removeNb(26) should return [[15, 21], [21, 15]]
+// or
+// removNb(26) should return [ {15, 21}, {21, 15} ]
+// or
+// removNb(26) should return "15 21, 21 15"
+// or
+
+// in C:
+// removNb(26) should return  {{15, 21}{21, 15}} tested by way of strings.
+// Function removNb should return a pointer to an allocated array of Pair pointers, each one also allocated. 
+// Note
+// See examples of returns for each language in "RUN SAMPLE TESTS"
+
+// FUNDAMENTALS MATHEMATICS PUZZLES
